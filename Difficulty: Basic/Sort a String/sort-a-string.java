20b1edb3a378b6eafbb5712cc7fa646613969b52
GyanaGRS <167738479+GyanaGRS@users.noreply.github.com>
2025-01-13 16:13:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sort a String/sort-a-string.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine().trim());
+        while(t-- > 0){
+            String s = sc.nextLine().trim();
+            Solution obj = new Solution();
+            System.out.println(obj.sort(s));
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+    String sort(String s) 
+    {
+        char[] sArr= s.toCharArray();
+        
+        Arrays.sort(sArr);
+        
+        return String.valueOf(sArr);
+    }
+} 
