310907ee5d85bc82928c81a1501d607102044692
GyanaGRS <167738479+GyanaGRS@users.noreply.github.com>
2025-01-16 11:51:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Repeated Character/repeated-character.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            String s;
+            s = sc.next();
+            
+            Solution ob = new Solution();
+            char res = ob.firstRep(s);
+            if (res == '#')
+                System.out.println(-1);
+            else
+                System.out.println(res);
+             
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    char firstRep(String S) {
+        // Iterate through the string
+        for (int i = 0; i < S.length(); i++) {
+            for (int j = i + 1; j < S.length(); j++) {
+                if (S.charAt(i) == S.charAt(j)) {
+                    return S.charAt(i); // Return the first repeated character
+                }
+            }
+        }
+        return '#'; // Return '#' if no repeated character is found
+    }
+}
